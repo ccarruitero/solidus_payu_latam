c966fce78afb59092ea07f6f6c918b917249d4cf
Cesar Carruitero <cesar@mozilla.pe>
2017-09-16 12:16:04 -0500

    keep same quotes style

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,7 @@ require 'rspec/rails'
 require 'database_cleaner'
 require 'ffaker'
 
-require "capybara/rspec"
+require 'capybara/rspec'
 require 'capybara/poltergeist'
 Capybara.register_driver(:poltergeist) do |app|
   Capybara::Poltergeist::Driver.new app, timeout: 90
