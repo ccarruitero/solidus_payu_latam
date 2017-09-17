cd75b0b8aafa19267c90f8fc73062292836c0350
Cesar Carruitero <cesar@mozilla.pe>
2017-09-17 00:22:42 -0500

    custom checkout flow

---
+++ app/models/order_decorator.rb
@@ -0,0 +1,15 @@
+Spree::Order.class_eval do
+  checkout_flow do
+    go_to_state :address
+    go_to_state :delivery
+    go_to_state :payment
+    go_to_state :complete
+  end
+
+  state_machine do
+    event :payment_failed do
+      reset
+      transition payment: :payment
+    end
+  end
+end
--- spec/models/spree/order_spec.rb
+++ spec/models/spree/order_spec.rb
@@ -8,4 +8,18 @@ describe Spree::Order, type: :model do
     order.customer_document = document
     expect(order.customer_document).to eq(document)
   end
+
+  context 'state_machine' do
+    it 'dont have confirm state' do
+      expect(Spree::Order.checkout_steps.keys.include?(:confirm)).to be(false)
+    end
+
+    context 'payment_failed event' do
+      it 'only have transition from complete' do
+        event = Spree::Order.state_machine.events[:payment_failed]
+        expect(event.known_states.include?(:confirm)).to be(false)
+        expect(event.known_states).to eq([:payment])
+      end
+    end
+  end
 end
