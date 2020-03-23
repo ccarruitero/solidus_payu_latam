1d6c64e0d4ce17c00b05f5ab8f3f5a6cf6242a1a
César Carruitero <ccarruitero@protonmail.com>
2020-03-23 10:59:38 -0500

    drop support for older solidus versions (< 2.3)

--- app/models/solidus/gateway/payu_latam_gateway.rb
+++ app/models/solidus/gateway/payu_latam_gateway.rb
@@ -1,49 +1,43 @@
 # frozen_string_literal: true
 
 module Solidus
-  class Gateway::PayuLatamGateway < ::Spree::Gateway
+  class Gateway::PayuLatamGateway < ::Spree::PaymentMethod::CreditCard
     preference :merchant_id, :string
     preference :account_id, :string
     preference :api_login, :string
     preference :api_key, :string
     preference :payment_country, :string, default: 'PE'
 
-    def provider_class
+    def gateway_class
       ActiveMerchant::Billing::PayuLatamGateway
     end
 
-    if SolidusSupport.solidus_gem_version < Gem::Version.new('2.3.x')
-      def method_type
-        'payu_latam'
-      end
-    else
-      def partial_name
-        'payu_latam'
-      end
+    def partial_name
+      'payu_latam'
     end
 
     def authorize(amount, credit_card, gateway_options)
       cvv = credit_card.verification_value
       options = add_missing_fields(gateway_options, cvv)
-      provider.authorize(amount, credit_card, options)
+      gateway.authorize(amount, credit_card, options)
     end
 
     def capture(amount, authorization, gateway_options)
-      provider.capture(amount, authorization, gateway_options)
+      gateway.capture(amount, authorization, gateway_options)
     end
 
     def void(authorization, gateway_options)
-      provider.void(authorization, gateway_options)
+      gateway.void(authorization, gateway_options)
     end
 
     def purchase(amount, credit_card, gateway_options)
       cvv = credit_card.verification_value
       options = add_missing_fields(gateway_options, cvv)
-      provider.purchase(amount, credit_card, options)
+      gateway.purchase(amount, credit_card, options)
     end
 
     def credit(amount, authorization, gateway_options)
-      provider.refund(amount, authorization, gateway_options)
+      gateway.refund(amount, authorization, gateway_options)
     end
 
     private
