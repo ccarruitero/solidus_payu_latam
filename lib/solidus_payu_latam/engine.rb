e0269e61b8f52ecd844662d92d1859261a709876
César Carruitero <ccarruitero@protonmail.com>
2020-03-23 10:01:46 -0500

    update cops

--- lib/solidus_payu_latam/engine.rb
+++ lib/solidus_payu_latam/engine.rb
@@ -9,7 +9,7 @@ module SolidusPayuLatam
     end
 
     def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')).sort.map do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
