98fc5e9e988166939ae378254d067985be25cc6c
César Carruitero <ccarruitero@protonmail.com>
2020-07-22 22:45:48 -0500

    feat: load views only when solidus backend or frontend is available

--- lib/solidus_payu_latam/engine.rb
+++ lib/solidus_payu_latam/engine.rb
@@ -4,6 +4,14 @@ module SolidusPayuLatam
   class Engine < Rails::Engine
     engine_name 'solidus_payu_latam'
 
+    if SolidusSupport.backend_available?
+      paths['app/views'] << 'lib/views/backend'
+    end
+
+    if SolidusSupport.frontend_available?
+      paths['app/views'] << 'lib/views/frontend'
+    end
+
     initializer 'spree.gateway.payment_methods', after: 'spree.register.payment_methods' do |app|
       app.config.spree.payment_methods << Solidus::Gateway::PayuLatamGateway
     end
